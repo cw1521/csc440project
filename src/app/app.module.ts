5d067b59ade03b60efbee1c996083e4e96c6371f
christian <cw1521@jagmail.southalabama.edu>
2019-09-30 23:46:43 -0500

    header and footer completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,17 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module'
+
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MatMenuModule, MatButtonModule } from '@angular/material/';
 import { AccodComponent } from './accod/accod.component';
 
 @NgModule({
   declarations: [
+  
     AppComponent,
     HomeComponent,
     HeaderComponent,
@@ -23,8 +26,7 @@ import { AccodComponent } from './accod/accod.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatMenuModule,
-    MatButtonModule
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+
+import {
+    MatButtonModule,
+    MatMenuModule
+} from '@angular/material';
+
+@NgModule({
+    imports: [
+        MatButtonModule,
+        MatMenuModule,
+    ],
+    exports: [
+        MatButtonModule,
+        MatMenuModule,
+    ]
+})
+export class MaterialModule {}
