ec647f37b908ae8d28e16b020d464c2824be1798
christian <cw1521@jagmail.southalabama.edu>
2019-11-02 19:21:16 -0500

    test endpoint

--- server.ts
+++ server.ts
@@ -70,7 +70,7 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//app.use(oktaAuth);
+app.use(oktaAuth);
 
 
 // if(process.env.NODE_ENV === 'production') {
